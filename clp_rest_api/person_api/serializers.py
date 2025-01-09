8ad7f04b0f2357448f44336c52573b0f7bd66bfc
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2025-01-09 03:04:03 -0800

    one to many cardinality again for worker database

--- clp_rest_api/person_api/serializers.py
+++ clp_rest_api/person_api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from .models import Worker
-
+from address_api.models import Location
+from address_api.serializers import LocationSerializer
 class WorkerSerializer(serializers.ModelSerializer):
+
+    #include address as a computed field
+    address = serializers.SerializerMethodField()
+
     class Meta:
         model = Worker
-        fields = ('id', 'name', 'age', 'company', 'registered_date',)
+        fields = ('id', 'name', 'age', 'company', 'registered_date', 'address',)
+
+    def get_address(self, obj):
+        # Retrieve all Location objects where entity_id matches the company ID and entity_type='company'
+        locations = Location.objects.filter(entity_id=obj.id, entity_type='person')
+        return LocationSerializer(locations, many=True).data if locations.exists() else []
