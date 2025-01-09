a6f9e82d03c40cedff10dcaa9d0636b3e9fe5b36
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2025-01-09 02:56:26 -0800

    added computed address to show one to many 1 company to multiple address

--- clp_rest_api/companies_api/serializers.py
+++ clp_rest_api/companies_api/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from .models import Company
+from address_api.models import Location
+from address_api.serializers import LocationSerializer
 
 class CompanySerializer(serializers.ModelSerializer):
+
+    #include address as a computed field
+    address = serializers.SerializerMethodField()
+
     class Meta:
         model = Company
-        fields = ('id', 'name', 'industry',)
+        fields = ('id', 'name', 'industry', 'address')
+
+    def get_address(self, obj):
+        # Retrieve all Location objects where entity_id matches the company ID and entity_type='company'
+        locations = Location.objects.filter(entity_id=obj.id, entity_type='company')
+        return LocationSerializer(locations, many=True).data if locations.exists() else []
